bd3ad037ff2793c475f8807195344464aabf8fe7
Mehmet Kaan Aksoy <mehmetkaanaksoy13@gmail.com>
2024-07-11 21:56:14 +0300

    Initial Commit

--- src/main/java/github/mkaksoy/Droplet.java
+++ src/main/java/github/mkaksoy/Droplet.java
@@ -1,17 +1,15 @@
 package github.mkaksoy;
 
-import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Droplet implements ClientModInitializer {
-	public static final String MOD_ID = "droplet";
-    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-
+public class Droplet implements ModInitializer {
+    public static final Logger LOGGER = LoggerFactory.getLogger("droplet");
 
 	@Override
-	public void onInitializeClient() {
-		LOGGER.info("Droplet is running!");
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
 	}
 }
